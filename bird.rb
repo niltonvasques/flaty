9032a7983135fd6e3e9122c0dda94b5f677d4443
Nilton Vasques <nilton.vasques@gmail.com>
2021-09-04 16:28:58 -0300

    Move bird to game object

--- bird.rb
+++ bird.rb
@@ -1,22 +1,20 @@
 require 'gosu'
 require 'ostruct'
+require './game_object'
 
-class Bird
+class Bird < GameObject
   FRAME_DURATION = 1000 / 60
   FG_SPEED = 30.0 / FRAME_DURATION
   SCALE = 3
 
   def initialize
-    @birds = Gosu::Image.load_tiles('assets/seagull_tiles.png', 32, 40)
+    bird_tiles = Gosu::Image.load_tiles('assets/seagull_tiles.png', 32, 40)
 
-    @bird = @birds[0]
+    super(tiles: bird_tiles, x: 0, y: GameWindow::SCREEN_HEIGHT / 2, current: 0,
+          scale_x: SCALE, scale_y: SCALE)
 
-    @scaled_width = @bird.width * SCALE
-    @scaled_height = @bird.width * SCALE
-
-    @move_x = 0
-    @move_y = GameWindow::SCREEN_HEIGHT / 2
-    @current = 0
+    @scaled_width = tiles[0].width * SCALE
+    @scaled_height = tiles[0].width * SCALE
 
     @elapsed = 0
   end
@@ -24,32 +22,15 @@ class Bird
   def update
     @elapsed += FRAME_DURATION
 
-    @current = (@elapsed / 200) % 4
+    self.current = (@elapsed / 200) % 4
 
-    @move_x -= 5 if Gosu.button_down? Gosu::KB_LEFT
-    @move_x += 5 if Gosu.button_down? Gosu::KB_RIGHT
-    @move_y -= 5 if Gosu.button_down? Gosu::KB_UP
-    @move_y += 5 if Gosu.button_down? Gosu::KB_DOWN
-    @move_x = 0 if @move_x < 0
-    @move_x = GameWindow::SCREEN_WIDTH - @scaled_width if @move_x + @scaled_width > GameWindow::SCREEN_WIDTH
-    @move_y = 0 if @move_y < 0
-    @move_y = GameWindow::SCREEN_HEIGHT - @scaled_height if @move_y + @scaled_height > GameWindow::SCREEN_HEIGHT
-  end
-
-  def draw
-    @birds[@current].draw(@move_x, @move_y, 0, scale_x = SCALE, scale_y = SCALE)
-  end
-
-  private
-
-  def build_layer(image, speed)
-    OpenStruct.new(
-      image: image,
-      scaled_width: image.width * @bg_scale_x,
-      x1: 0,
-      x2: (image.width * @bg_scale_x),
-      y: GameWindow::SCREEN_HEIGHT - (image.height * @bg_scale_y),
-      speed: speed
-    )
+    self.x -= 5 if Gosu.button_down? Gosu::KB_LEFT
+    self.x += 5 if Gosu.button_down? Gosu::KB_RIGHT
+    self.y -= 5 if Gosu.button_down? Gosu::KB_UP
+    self.y += 5 if Gosu.button_down? Gosu::KB_DOWN
+    self.x = 0 if self.x < 0
+    self.x = GameWindow::SCREEN_WIDTH - @scaled_width if self.x + @scaled_width > GameWindow::SCREEN_WIDTH
+    self.y = 0 if self.y < 0
+    self.y = GameWindow::SCREEN_HEIGHT - @scaled_height if self.y + @scaled_height > GameWindow::SCREEN_HEIGHT
   end
 end
--- game_object.rb
+++ game_object.rb
@@ -8,6 +8,8 @@ class GameObject < OpenStruct
   end
 
   def draw
-    image.draw(x, y, z, scale_x = self.scale_x, scale_y = self.scale_y)
+    return image.draw(x, y, z, scale_x = self.scale_x, scale_y = self.scale_y) if image
+
+    tiles[current].draw(x, y, 0, scale_x = self.scale_x, scale_y = self.scale_y) if tiles
   end
 end
