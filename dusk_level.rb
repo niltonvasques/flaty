6bab644c87f53554447fa4e2dc481b2bfdab0f64
Nilton Vasques <nilton.vasques@gmail.com>
2021-09-05 03:00:25 -0300

    move level code from game class to DuskLevel class

---
+++ dusk_level.rb
@@ -0,0 +1,82 @@
+require 'gosu'
+require 'pry-byebug'
+require './background'
+require './bird'
+require './star'
+
+module ZLayers
+  BG, STARS, PLAYER, UI = *0..3
+end
+
+class DuskLevel
+  SCREEN_WIDTH   = 1280
+  SCREEN_HEIGHT  = 720
+
+  def initialize
+    # assets
+    @star_anim = Gosu::Image.load_tiles("assets/star.png", 25, 25)
+    @font = Gosu::Font.new(20)
+    @song = Gosu::Song.new('assets/sounds/dusk_theme.mp3')
+    @song.play
+
+    # state
+    @paused = false
+    @paused_at = 0
+
+    # objects
+    @background = Background.new
+    @bird = Bird.new
+    @stars = Array.new
+  end
+
+  def update
+    return if paused?
+
+    @bird.update
+    @stars.each { |star| star.update(@bird.speed) }
+    @background.update(@bird.speed)
+
+    @bird.collect_stars(@stars)
+
+    if rand(100) < 4 and @stars.size < 10
+      @stars.push(Star.new(@star_anim))
+    end
+
+    @stars.reject! { |star| star.x < 0 }
+  end
+
+  def draw
+    draw_ui
+
+    @background.draw
+
+    return if @paused
+
+    @bird.draw
+    @stars.each { |star| star.draw }
+  end
+
+  private
+
+  def draw_ui
+    @font.draw_text("FPS: #{Gosu.fps}", 10, 10, ZLayers::UI, 1.0, 1.0, Gosu::Color::GREEN)
+    @font.draw_text("Score: #{@bird.score}", SCREEN_WIDTH - 100, 10, ZLayers::UI, 1.0, 1.0, Gosu::Color::RED)
+  end
+
+  def paused?
+    if Gosu.button_down? Gosu::KB_P
+      if (Gosu.milliseconds - @paused_at) > 1000
+        @paused = !@paused
+        @paused_at = Gosu.milliseconds
+        if @paused
+          @song.pause
+          @bird.pause
+        else
+          @song.play
+          @bird.play
+        end
+      end
+    end
+    @paused
+  end
+end
