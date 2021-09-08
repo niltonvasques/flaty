a2c570edfde21732c0eb5df5682e093b78ad998d
Nilton Vasques <nilton.vasques@gmail.com>
2021-09-08 03:56:44 -0300

    playing with acceleration

--- bird.rb
+++ bird.rb
@@ -7,7 +7,8 @@ require './rect'
 class Bird < GameObject
   IDLE = 2.freeze
   IDLE_SPEED = Vector2d[IDLE, 0].freeze
-  SPEED = 10.freeze # units per second
+  ACCELERATION = 20.freeze # units per second
+  SPEED = 10.freeze
   SCALE = 3.freeze
   FRAMES = 4.freeze
   LEFT_FRAMES_INDEX   = 6.freeze
@@ -21,7 +22,8 @@ class Bird < GameObject
     play
 
     super(position: Vector2d[1, 14], z: ZLayers::PLAYER, scale_x: SCALE, scale_y: SCALE,
-          speed: IDLE_SPEED.dup, score: 0, tiles: bird_tiles, current: 0, debug: Gosu::Color::RED)
+          speed: IDLE_SPEED.dup, max_speed: Vector2d[SPEED, SPEED], damp: 0.8,
+          score: 0, tiles: bird_tiles, current: 0, debug: Gosu::Color::RED)
   end
 
   def update
@@ -31,11 +33,16 @@ class Bird < GameObject
   end
 
   def update_speed
-    self.speed =  IDLE_SPEED.dup
-    self.speed =  Vector2d[-SPEED, 0] if Gosu.button_down? Gosu::KB_LEFT
-    self.speed += Vector2d[SPEED,  0] if Gosu.button_down? Gosu::KB_RIGHT
-    self.speed += Vector2d[0, -SPEED] if Gosu.button_down? Gosu::KB_UP
-    self.speed += Vector2d[0,  SPEED] if Gosu.button_down? Gosu::KB_DOWN
+    self.acceleration = Vector2d[0,  0]
+    self.acceleration += Vector2d[-ACCELERATION, 0] if Gosu.button_down? Gosu::KB_LEFT
+    self.acceleration += Vector2d[ACCELERATION,  0] if Gosu.button_down? Gosu::KB_RIGHT
+    self.acceleration += Vector2d[0, -ACCELERATION] if Gosu.button_down? Gosu::KB_UP
+    self.acceleration += Vector2d[0,  ACCELERATION] if Gosu.button_down? Gosu::KB_DOWN
+    #self.speed =  IDLE_SPEED.dup
+    #self.speed =  Vector2d[-SPEED, 0] if Gosu.button_down? Gosu::KB_LEFT
+    #self.speed += Vector2d[SPEED,  0] if Gosu.button_down? Gosu::KB_RIGHT
+    #self.speed += Vector2d[0, -SPEED] if Gosu.button_down? Gosu::KB_UP
+    #self.speed += Vector2d[0,  SPEED] if Gosu.button_down? Gosu::KB_DOWN
 
     frame_duration = self.speed.x.abs <= IDLE ? FRAME_DURATION : FRAME_FAST_DURATION
     self.current = (Gosu.milliseconds / frame_duration) % FRAMES
--- game_object.rb
+++ game_object.rb
@@ -8,9 +8,9 @@ class GameObject < OpenStruct
 
   def initialize(opts = {})
     default = {
-      position: Vector2d[0,0], z: 0, previous_position: Vector2d[0,0],
+      position: Vector2d[0,0], z: 0, previous_position: Vector2d[0,0], acceleration: Vector2d[0,0],
       speed: Vector2d[0,0], scale_x: 1, scale_y: 1, current: 0, camera: true, debug: false,
-      rect: Rect[0, 0, 0, 0]
+      rect: Rect[0, 0, 0, 0], max_speed: Vector2d[0, 0], damp: 0
     }
     super(default.merge(opts))
     self.previous_position = self.position.dup
@@ -34,6 +34,14 @@ class GameObject < OpenStruct
 
   def update
     self.previous_position = self.position.dup
+
+    self.speed += self.acceleration * GameWindow.delta
+    self.speed.x = self.max_speed.x if self.speed.x > 0 and self.speed.x.abs > self.max_speed.x
+    self.speed.x = -self.max_speed.x if self.speed.x < 0 and self.speed.x.abs > self.max_speed.x
+    self.speed.y = self.max_speed.y if self.speed.y > 0 and self.speed.y.abs > self.max_speed.y
+    self.speed.y = -self.max_speed.y if self.speed.y < 0 and self.speed.y.abs > self.max_speed.y
+    self.speed.y *= self.damp if self.acceleration.y == 0
+    self.speed.x *= self.damp if self.acceleration.x == 0
 
     self.position += self.speed * GameWindow.delta
   end
