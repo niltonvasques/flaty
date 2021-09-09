97176663523cf771ac01b1347be7ac0cc98f09c2
Nilton Vasques <nilton.vasques@gmail.com>
2021-09-09 03:35:48 -0300

    add bob steps sound

--- bob.rb
+++ bob.rb
@@ -7,28 +7,30 @@ require './rect'
 class Bob < GameObject
   IDLE_SPEED = Vector2d[0, 0].freeze
   ACCELERATION = 100.freeze # units per second
+  GRAVITY = Vector2d[0, 9.8].freeze
+  JUMP_ACCELERATION = Vector2d[0,-110].freeze # 110 m/s
   SPEED = 5.freeze
   SCALE = 2.freeze
   FRAMES = 5.freeze
-  JUMP_DURATION = 50.freeze
+  JUMP_DURATION = 50.freeze # 50 ms
   RIGHT_FRAMES_INDEX   = 6.freeze
   STEPS_PER_SECOND    = 3.freeze
   FRAME_DURATION      = (1000 / (STEPS_PER_SECOND * 5)).freeze
   FRAME_FAST_DURATION = 80.freeze
-  GRAVITY = Vector2d[0, 9.8].freeze
   TERMINAL_SPEED = 55.freeze # 50 m/s
   IDLE_FRAME = 5.freeze
 
   def initialize
-    bird_tiles = Gosu::Image.load_tiles('assets/bob.png', 26, 30, tileable: true)
-    @wings = Gosu::Sample.new('assets/sounds/dragonflap.mp3')
+    bob_tiles = Gosu::Image.load_tiles('assets/bob.png', 26, 30, tileable: true)
+    @steps = Gosu::Sample.new('assets/sounds/steps.wav')
     @beep = Gosu::Sample.new('assets/sounds/beep.wav')
     @jump_at = -2000
     @jumping = false
+    play
 
     super(position: Vector2d[1, 14], z: ZLayers::PLAYER, scale_x: SCALE, scale_y: SCALE,
           speed: IDLE_SPEED.dup, max_speed: Vector2d[SPEED, TERMINAL_SPEED], damp: 0.8,
-          score: 0, tiles: bird_tiles, current: 0, debug: Gosu::Color::RED,
+          score: 0, tiles: bob_tiles, current: 0, debug: Gosu::Color::RED,
           state: :idle, face: :right)
   end
 
@@ -56,30 +58,33 @@ class Bob < GameObject
       @jump_at = Gosu.milliseconds
       @jumping = true
       self.state = :jumping
-      self.acceleration += Vector2d[0, -110]
+      self.acceleration += JUMP_ACCELERATION
     end
     if (Gosu.milliseconds - @jump_at) > JUMP_DURATION
       @jumping = false
     else
-      self.acceleration += Vector2d[0, -110]
+      self.acceleration += JUMP_ACCELERATION
     end
 
-
-    update_state
+    update_direction
 
     case self.state
     when :idle
       self.current = IDLE_FRAME
+      pause
     when :jumping
+      pause
       self.current = 4
     else
       self.current = (Gosu.milliseconds / FRAME_DURATION) % FRAMES
     end
 
+    play if self.state == :walking
+
     self.current += RIGHT_FRAMES_INDEX if self.face == :right
   end
 
-  def update_state
+  def update_direction
     self.face = :right if self.acceleration.x > 0
     self.face = :left if self.acceleration.x < 0
   end
@@ -103,14 +108,19 @@ class Bob < GameObject
   end
 
   def pause
-    @wings_playing.pause
+    if !@steps_playing.nil? and !@steps_playing.paused?
+      @steps_playing.pause
+    end
   end
 
   def play
-    volume  = 2
-    speed   = 2
-    looping = true
-    @wings_playing = @wings.play(volume, speed, looping)
+    if @steps_playing.nil? or @steps_playing.paused?
+      return @steps_playing.resume if @steps_playing
+      volume  = 2
+      speed   = 1.5
+      looping = true
+      @steps_playing = @steps.play(volume, speed, looping)
+    end
   end
 
   def turn_left?
