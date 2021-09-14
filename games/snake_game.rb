a3eb4e3a1da06ea44b17ec1e45fea292687aae29
Nilton Vasques <nilton.vasques@gmail.com>
2021-09-14 01:06:08 -0300

    move to games folder

--- games/snake_game.rb
+++ games/snake_game.rb
@@ -1,5 +1,5 @@
-unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
-  $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__) + '/..'))
+  $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/..'))
 end
 
 require 'pry-byebug'
