afde56cae9eaeeec560f1d61f1c1636059e6e800
Nilton Vasques <nilton.vasques@gmail.com>
2021-09-30 00:05:52 -0300

    Porting math axis

--- games/math_axis.cr
+++ games/math_axis.cr
@@ -1,4 +1,5 @@
 require "flaty"
+require "flaty/math/calculus"
 
 class MathAxis < Flaty::GameWindow
   SCREEN_WIDTH        = 1500
@@ -19,7 +20,7 @@ class MathAxis < Flaty::GameWindow
 
     @points   = [[0,2],[1,1],[2,2],[3,0],[4,3],[5,0],[6,3],[7,1]]
     @fps_list = [] of Float64
-    @px       = 0
+    @px       = 0.0
     @label_y  = 0
 
     ## assets
@@ -39,6 +40,8 @@ class MathAxis < Flaty::GameWindow
     draw_fx("1/(1+e^-x)", Flaty::Colors::RED) { |x| 1.0/(1+Math.exp(-x)) }
     draw_fx("e^x", Flaty::Colors::YELLOW)     { |x| Math.exp(-x) }
 
+    draw_fx_and_dydx("sin(x²)") { |x| Math.sin(x**2) }
+
     @fps_list << (1.0/@delta.as_seconds).round(2)
     @fps_list = @fps_list[2..@fps_list.size] if @fps_list.size > 1000
     fps = "FPS: #{(@fps_list.sum / @fps_list.size).to_i}"
@@ -48,8 +51,10 @@ class MathAxis < Flaty::GameWindow
   def button_down(code)
     @camera.key_pressed(self, code)
 
-    #  move_derivative(-1) if Gosu.button_down? Gosu::KB_A
-    #  move_derivative(1)  if Gosu.button_down? Gosu::KB_D
+    case code
+    when .a? then @px -= 0.1
+    when .d? then @px += 0.1
+    end
   end
 
   #def animate_polynomial
@@ -88,30 +93,29 @@ class MathAxis < Flaty::GameWindow
   #  puts t
   end
 
-  #def draw_fx_and_dydx(&block)
-  #  label = format_equation(block.source.scan(/\{ \|x\| (.*) \}/).flatten.first)
-  #  draw_fx(Gosu::Color::BLACK, label: label) { |x| block.call(x) }
-  #  dxdy_label = "dy/dx #{label}"
-  #  draw_fx(Gosu::Color::WHITE, bold: false, label: dxdy_label) do |x|
-  #    Calculus.derivative_line(x, @px, block)
-  #  end
-  #end
+  def draw_fx_and_dydx(label, &block : Float64 -> Float64)
+    draw_fx(label, Flaty::Colors::BLACK) { |x| block.call(x) }
+    dxdy_label = "dy/dx #{label}"
+    draw_fx(dxdy_label, Flaty::Colors::WHITE) do |x|
+      Calculus.derivative_line(x, @px.to_f, block)
+    end
+  end
 
   LINE_THICKNESS = 3
   MIN_PRECISION = 0.000000000001
-  def draw_fx(label, color)
+  def draw_fx(label, color, &block : Float64 -> Float64)
   #  opts = { bold: true }.merge(opts)
     precision = Math.max((@camera.width / 1000.0).abs, MIN_PRECISION)
 
     x2 = @camera.rect.x
     x1 = x2
-    y1 = yield(x1)
+    y1 = block.call(x1)
 
     while x2 <= (@camera.rect.x + @camera.width)
-      y2 = yield(x2)
+      y2 = block.call(x2)
       w = (x2 - x1)
       h = (y2 - y1)
-      puts "#{x1} x1 #{y1} x2 #{x2} x2 #{y2} y2"
+      #puts "#{x1} x1 #{y1} x2 #{x2} x2 #{y2} y2"
       Flaty.draw_line(x1, y1, x2, y2, color)
       x1 = x2
       y1 = y2
