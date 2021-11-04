d2cddf14a0fd98c65417cc63a391db8d0e8eaf9c
Nilton Vasques <nilton.vasques@gmail.com>
2021-11-04 02:49:53 -0300

    drawing line from stdin arguments

--- samples/math_axis.cr
+++ samples/math_axis.cr
@@ -9,7 +9,7 @@ class MathAxis < Flaty::GameWindow
   CAMERA_HEIGHT_UNITS = 10.0
   SCALE               = SCREEN_WIDTH / CAMERA_WIDTH_UNITS
 
-  def initialize
+  def initialize(@dataset = [] of {Float64, Float64})
     super(CAMERA_WIDTH_UNITS, CAMERA_HEIGHT_UNITS, SCALE, "Math Axis")
 
     ## assets
@@ -32,10 +32,15 @@ class MathAxis < Flaty::GameWindow
     @camera_debug.draw
 
     @label_y = 0
-    #draw_fx("sin(x)", Flaty::Colors::GREEN)   { |x| Math.sin(x) }
-    draw_fx("2/(1+e^(-2x))-1", Flaty::Colors::YELLOW) { |x| (2.0/(1+Math.exp(-2*x))) - 1 }
-    draw_fx("1/(1+e^-x)", Flaty::Colors::RED) { |x| 1.0/(1+Math.exp(-x)) }
-    draw_fx("e^x", Flaty::Colors::BLUE)     { |x| Math.exp(-x) }
+    draw_dataset
+    if @dataset.empty?
+      #draw_fx("sin(x)", Flaty::Colors::GREEN)   { |x| Math.sin(x) }
+      draw_fx("1/(1+e^-x) sigmoid", Flaty::Colors::BLUE) { |x| 1.0/(1+Math.exp(-x)) }
+      draw_fx("2/(1+e^(-2x))-1 tanh", Flaty::Colors::YELLOW) { |x| (2.0/(1+Math.exp(-2*x))) - 1 }
+      #draw_fx("Leaky ReLU", Flaty::Colors::BLACK)     { |x| (x >= 0 ? x : 0.01 * x) }
+      draw_fx("max(0,x) ReLU", Flaty::Colors::GREEN)     { |x| Math.max(0, x).to_f }
+      draw_fx("e^x", Flaty::Colors::RED)     { |x| Math.exp(-x) }
+    end
 
     #draw_fx_and_dydx("sin(x²)") { |x| Math.sin(x**2) }
     #draw_fx_and_dydx("sin(x)", Flaty::Colors::GREEN) { |x| Math.sin(x) }
@@ -62,6 +67,23 @@ class MathAxis < Flaty::GameWindow
     dxdy_label = "dy/dx #{label}"
     draw_fx(dxdy_label, Flaty::Colors::WHITE) do |x|
       Calculus.derivative_line(x, @px.to_f, block)
+    end
+  end
+
+  def draw_dataset
+    return if @dataset.empty?
+
+    max = @dataset.map { |row| row[0] }.max
+
+    x1 = (@dataset[0][0] / max) * 5
+    y1 = @dataset[0][1] * 5
+    @dataset[1..-1].each do |tuple|
+      x2, y2 = tuple
+      x2 = (x2 / max) * 5
+      y2 *= 5
+      Flaty.draw_line(x1, y1, x2, y2, Flaty::Colors::GREEN)
+      x1 = x2
+      y1 = y2
     end
   end
 
@@ -95,5 +117,14 @@ class MathAxis < Flaty::GameWindow
   end
 end
 
-game = MathAxis.new
+dataset = [] of {Float64, Float64}
+if ARGV.size > 0 && ARGV[0] == "-stdin"
+  while line = gets
+    puts "read: #{line}"
+    entries = line.split(",")
+    dataset << {entries[0].to_f, entries[1].to_f}
+  end
+end
+
+game = MathAxis.new(dataset)
 game.loop
